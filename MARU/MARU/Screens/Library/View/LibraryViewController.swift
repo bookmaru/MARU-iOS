7d4692f3bf915444a0acaadb1c38735222e68cf7
오준현 <oh.junhyeon95@gmail.com>
2021-04-03 20:58:10 +0900

    :seedling: 탭바 컨트롤러, 관련된 뷰 생성

---
+++ MARU/MARU/Screens/Library/View/LibraryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LibraryViewController.swift
+//  MARU
+//
+//  Created by 오준현 on 2021/04/03.
+//
+
+import UIKit
+
+final class LibraryViewController: BaseViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
