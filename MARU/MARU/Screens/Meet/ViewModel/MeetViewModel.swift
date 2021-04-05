ea74ae6dba1b9d2b4ca43a7176cc2f50d105b369
오준현 <oh.junhyeon95@gmail.com>
2021-04-05 21:55:29 +0900

    :sparkles: MeetViewModel 생성

---
+++ MARU/MARU/Screens/Meet/ViewModel/MeetViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  MeetViewModel.swift
+//  MARU
+//
+//  Created by 오준현 on 2021/04/05.
+//
+
+final class MeetViewModel: ViewModelType {
+
+  struct Input {
+
+  }
+
+  struct Output {
+
+  }
+
+  func transform(input: Input) -> Output {
+
+    return Output()
+  }
+
+}
