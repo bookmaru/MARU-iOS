7624dce2baed0d1d9f7090283b646b5f352d2db2
오준현 <oh.junhyeon95@gmail.com>
2021-04-08 22:13:37 +0900

    :construction: CollectionView 레이아웃 잡아놓음

--- MARU/MARU/Screens/Meet/View/MeetViewController.swift
+++ MARU/MARU/Screens/Meet/View/MeetViewController.swift
@@ -9,8 +9,51 @@ import UIKit
 
 final class MeetViewController: BaseViewController {
 
+  private let collectionView: UICollectionView = {
+    let layout = UICollectionViewFlowLayout()
+    layout.scrollDirection = .horizontal
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+    collectionView.backgroundColor = .clear
+    return collectionView
+  }()
+
+  private let viewModel: MeetViewModel = {
+    let viewModel = MeetViewModel()
+
+    return viewModel
+  }()
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    layout()
+    bind(reactor: viewModel)
+  }
+}
+
+extension MeetViewController {
+  func layout() {
+    view.add(collectionView) {
+      $0.rx.setDelegate(self)
+        .disposed(by: self.disposeBag)
+      $0.snp.makeConstraints {
+        $0.edges.equalTo(self.view.safeAreaLayoutGuide)
+      }
+    }
+  }
+
+  func bind(reactor: MeetViewModel) {
+//    reactor.state.map {
+//      $0.data
+//    }.distinctUntilChanged()
+    
+  }
+}
+
+extension MeetViewController: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: 100, height: 100)
   }
 }
