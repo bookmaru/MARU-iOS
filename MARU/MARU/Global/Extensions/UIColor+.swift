77377430bcc934575127c8d02c7624e58bf46e3d
5anniversary <oh.junhyeon95@gmail.com>
2021-04-17 17:48:49 +0900

    Color Extension 추가

---
+++ MARU/MARU/Global/Extensions/UIColor+.swift
@@ -0,0 +1,67 @@
+//
+//  UIColor+.swift
+//  MARU
+//
+//  Created by JH_OH on 2021/04/17.
+//
+
+import UIKit
+
+extension UIColor {
+  @nonobjc class var black: UIColor {
+    return UIColor(white: 52.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var veryLightPink: UIColor {
+    return UIColor(white: 209.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var veryLightPinkTwo: UIColor {
+    return UIColor(white: 235.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var brownishGrey: UIColor {
+    return UIColor(white: 112.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var blackTwo: UIColor {
+    return UIColor(white: 44.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var orangeyRed: UIColor {
+    return UIColor(red: 1.0, green: 60.0 / 255.0, blue: 52.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var pastelRed: UIColor {
+    return UIColor(red: 224.0 / 255.0, green: 83.0 / 255.0, blue: 78.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var brownGrey: UIColor {
+    return UIColor(white: 178.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var menuBarWhite: UIColor {
+    return UIColor(white: 250.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var cornflowerBlue: UIColor {
+    return UIColor(red: 65.0 / 255.0, green: 105.0 / 255.0, blue: 225.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var white: UIColor {
+    return UIColor(white: 1.0, alpha: 1.0)
+  }
+
+  @nonobjc class var brownGreyTwo: UIColor {
+    return UIColor(white: 159.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var veryLightPinkThree: UIColor {
+    return UIColor(white: 240.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var black22: UIColor {
+    return UIColor(white: 34.0 / 255.0, alpha: 0.22)
+  }
+
+}
--- MARU/MARU/Global/Resources/Image.swift
+++ MARU/MARU/Global/Resources/Image.swift
@@ -5,4 +5,14 @@
 //  Created by JH_OH on 2021/04/17.
 //
 
-import Foundation
+import UIKit
+
+enum Image {
+  static let appIcon = UIImage(named: "AppIcon")
+  static let leftQuotationMark = UIImage(named: "quotationMarkLeft")
+  static let rightQuotationMark = UIImage(named: "quotationMarkRight")
+  static let meetGradient = UIImage(named: "gradientImage")
+  static let loginLogo = UIImage(named: "loginLogo")
+  static let bookImgBlack = UIImage(named: "bookImgBlack")
+  static let bookmark = UIImage(named: "bookmark")
+}
