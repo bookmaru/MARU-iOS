b5c9eaff0fff1f960f49e76d781bffe0d21e4308
오준현 <oh.junhyeon95@gmail.com>
2021-04-20 23:44:10 +0900

    :seedling: 채팅 뷰, 뷰모델 생성

---
+++ MARU/MARU/Screens/Chat/View/ChatViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ChatViewController.swift
+//  MARU
+//
+//  Created by 오준현 on 2021/04/20.
+//
+
+import UIKit
+
+final class ChatViewController: BaseViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+}
