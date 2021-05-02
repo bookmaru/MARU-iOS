01ca2da39aaaa9e6419ad1f7e8f3e686f700595a
오준현 <oh.junhyeon95@gmail.com>
2021-05-02 15:17:24 +0900

    :construction: 간단하게 버튼 눌렀을때 text 스트림 연결

--- MARU/MARU/Screens/Chat/View/ChatViewController.swift
+++ MARU/MARU/Screens/Chat/View/ChatViewController.swift
@@ -11,17 +11,20 @@ final class ChatViewController: BaseViewController {
 
   private let collectionView: UICollectionView = {
     let layout = UICollectionViewFlowLayout()
-
+    layout.minimumLineSpacing = 0
+    layout.minimumInteritemSpacing = 0
     let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-    collectionView.register(ChatCollectionViewCell.self,
-                            forCellWithReuseIdentifier: ChatCollectionViewCell.reuseIdentifier)
+    collectionView.register(cell: ChatCollectionViewCell.self)
     collectionView.backgroundColor = .white
     return collectionView
   }()
 
+  private let bottomView = InputView()
+
   override func viewDidLoad() {
     super.viewDidLoad()
     layout()
+    bind()
   }
 
   override func viewWillAppear(_ animated: Bool) {
@@ -34,13 +37,26 @@ final class ChatViewController: BaseViewController {
 
 extension ChatViewController {
   private func layout() {
+    view.add(bottomView) {
+      $0.snp.makeConstraints {
+        $0.leading.bottom.trailing.equalTo(self.view.safeAreaLayoutGuide)
+        $0.height.equalTo(50)
+      }
+    }
     view.add(collectionView) {
       $0.snp.makeConstraints {
-        $0.edges.equalTo(self.view.safeAreaLayoutGuide)
+        $0.leading.trailing.top.equalTo(self.view.safeAreaLayoutGuide)
+        $0.bottom.equalTo(self.bottomView.snp.top)
       }
       $0.delegate = self
       $0.dataSource = self
     }
+  }
+
+  private func bind() {
+    bottomView.rx.didTapSendButton.subscribe(onNext: { text in
+      print(text)
+    }).disposed(by: disposeBag)
   }
 }
 
@@ -48,7 +64,7 @@ extension ChatViewController: UICollectionViewDelegateFlowLayout {
   func collectionView(_ collectionView: UICollectionView,
                       layout collectionViewLayout: UICollectionViewLayout,
                       sizeForItemAt indexPath: IndexPath) -> CGSize {
-    return CGSize(width: 100, height: 100)
+    return CGSize(width: view.frame.width, height: 100)
   }
 }
 
@@ -61,7 +77,10 @@ extension ChatViewController: UICollectionViewDataSource {
   func collectionView(_ collectionView: UICollectionView,
                       cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     let cell: ChatCollectionViewCell = collectionView.dequeueReusableCell(forIndexPath: indexPath)
-
+    cell.backgroundColor = UIColor.red.withAlphaComponent(0.3)
+    if indexPath.item % 2 == 0 {
+      cell.backgroundColor = UIColor.blue.withAlphaComponent(0.3)
+    }
     return cell
   }
 }
