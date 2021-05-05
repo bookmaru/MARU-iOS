ba9a431173847e2771644bb0b7a58f49d29a1b5b
오준현 <oh.junhyeon95@gmail.com>
2021-05-05 15:28:05 +0900

    :seedling: add MyChat, OtherChatCell

---
+++ MARU/MARU/Screens/Chat/View/Cell/MyChatCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MyChatCell.swift
+//  MARU
+//
+//  Created by 오준현 on 2021/05/05.
+//
+
+import UIKit
+
+class MyChatCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ MARU/MARU/Screens/Chat/View/Cell/OtherChatCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  OtherChatCollectionViewCell.swift
+//  MARU
+//
+//  Created by 오준현 on 2021/05/05.
+//
+
+import UIKit
+
+class OtherChatCollectionViewCell: UICollectionViewCell {
+    
+}
