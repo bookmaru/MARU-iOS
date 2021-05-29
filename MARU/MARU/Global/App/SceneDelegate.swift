47b0d37136a6c37c13e8b3baff3a225c1553b3c9
KIN <psychehose@gmail.com>
2021-05-30 00:02:41 +0900

    sceneDelegate 설정

--- MARU/MARU/Global/App/SceneDelegate.swift
+++ MARU/MARU/Global/App/SceneDelegate.swift
@@ -18,8 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   ) {
     guard let windowScene = (scene as? UIWindowScene) else { return }
     let window = UIWindow(windowScene: windowScene)
-    window.rootViewController = UINavigationController(rootViewController: MainViewController())
-//    window.rootViewController = SplashViewController()
+    window.rootViewController = SplashViewController()
     window.makeKeyAndVisible()
     self.window = window
   }
