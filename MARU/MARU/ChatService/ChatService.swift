b846bb0ea23c9a7f220812758cf025f742831599
오준현 <oh.junhyeon95@gmail.com>
2021-08-16 11:50:49 +0900

    :heavy_plus_sign: 소켓 라이브러리 수정

--- MARU/MARU/ChatService/ChatService.swift
+++ MARU/MARU/ChatService/ChatService.swift
@@ -5,31 +5,31 @@
 //  Created by 오준현 on 2021/08/15.
 //
 
-import SocketIO
+// import SocketIO
 import RxSwift
 
 final class ChatService {
 
-  private let baseURL: String = "http://3.36.251.65:8081/"
+//  private let baseURL: String = "http://3.36.251.65:8081/"
   private let roomIndex: String
 
-  var socket: SocketIOClient
-  var manager = SocketManager(
-    socketURL: URL(string: "http://3.36.251.65:8081/regiter")!,
-    config: [.log(true), .compress]
-  )
+//  var socket: SocketIOClient
+//  var manager = SocketManager(
+//    socketURL: URL(string: "http://3.36.251.65:8081/regiter")!,
+//    config: [.log(true), .compress]
+//  )
 
   init(roomIndex: String) {
     self.roomIndex = roomIndex
-    socket = manager.defaultSocket
-
-    socket.connect()
-
-    manager = SocketManager(socketURL: URL(string: baseURL + "oo/aa")!, config: [.log(true), .compress])
-    socket = manager.socket(forNamespace: "/")
-
-    socket.connect()
-    socket.emit("", "")
+//    socket = manager.defaultSocket
+//
+//    socket.connect()
+//
+//    manager = SocketManager(socketURL: URL(string: baseURL + "oo/aa")!, config: [.log(true), .compress])
+//    socket = manager.socket(forNamespace: "/")
+//
+//    socket.connect()
+//    socket.emit("", "")
   }
 
 }
