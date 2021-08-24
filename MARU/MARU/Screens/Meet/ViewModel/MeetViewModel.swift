43faae1be29f8ad34af5cb6825cd4ee4c9209493
오준현 <oh.junhyeon95@gmail.com>
2021-08-24 17:22:21 +0900

    :monocle_face: 테스트 할수있는 구조가...아니라 우선 service 연결만

--- MARU/MARU/Screens/Meet/ViewModel/MeetViewModel.swift
+++ MARU/MARU/Screens/Meet/ViewModel/MeetViewModel.swift
@@ -9,14 +9,33 @@ import RxCocoa
 import RxSwift
 
 final class MeetViewModel {
+
   struct Input {
-
+    let viewDidLoadPublisher: PublishSubject<Void>
   }
+
   struct Output {
-
+    let group: Driver<[MeetCase]>
   }
 
+  let disposeBag = DisposeBag()
   func transform(input: Input) -> Output {
-    return Output()
+    let output = input.viewDidLoadPublisher
+      .flatMap { NetworkService.shared.group.participateList() }
+      .map { $0.data.map { $0.groups } }
+      .compactMap { $0 }
+      .map { groups -> [MeetCase] in
+        if groups.isEmpty {
+          return [.empty]
+        }
+        let meet = groups.map { MeetCase.meet($0) }
+        if meet.count < 3 {
+          return meet + [.empty]
+        }
+        return meet
+      }
+      .asDriver(onErrorJustReturn: [])
+
+    return Output(group: output)
   }
 }
