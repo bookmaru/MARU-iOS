e51091d38406a40c34fc11262842721877776a75
오준현 <oh.junhyeon95@gmail.com>
2021-09-04 01:40:47 +0900

    :lipstick: bottom tab bar setting

--- MARU/MARU/Screens/Base/TabBarController.swift
+++ MARU/MARU/Screens/Base/TabBarController.swift
@@ -13,32 +13,47 @@ final class TabBarController: UITabBarController {
     super.viewDidLoad()
 
     tabbar()
+    clearShadow()
+    tabBar.layer.applyShadow(color: .black, alpha: 0.2, shadowX: 0, shadowY: 0, blur: 4)
   }
 }
 
 extension TabBarController {
   private func tabbar() {
     let mainView = MainViewController()
-    let mainViewTabItem = UITabBarItem(title: "메인",
-                                       image: UIImage(),
-                                       selectedImage: UIImage())
+    let mainViewTabItem = UITabBarItem(
+      title: "메인",
+      image: Image.mainTabbarHome,
+      selectedImage: Image.mainTabbarHomeSelected
+    )
     mainView.tabBarItem = mainViewTabItem
     let mainViewController = UINavigationController(rootViewController: mainView)
 
     let meetView = MeetViewController()
-    let meetViewTabItem = UITabBarItem(title: "모임",
-                                       image: UIImage(),
-                                       selectedImage: UIImage())
+    let meetViewTabItem = UITabBarItem(
+      title: "모임",
+      image: Image.mainTabbarMoim,
+      selectedImage: Image.mainTabbarMoimSelected
+    )
     meetView.tabBarItem = meetViewTabItem
     let meetViewController = UINavigationController(rootViewController: meetView)
 
     let libraryView = MyLibraryViewController()
-    let libraryViewTabItem = UITabBarItem(title: "서재",
-                                          image: UIImage(),
-                                          selectedImage: UIImage())
+    let libraryViewTabItem = UITabBarItem(
+      title: "서재",
+      image: Image.mainTabbarBook,
+      selectedImage: Image.mainTabbarBookSelected
+    )
     libraryView.tabBarItem = libraryViewTabItem
     let libraryViewController = UINavigationController(rootViewController: libraryView)
 
     viewControllers = [mainViewController, meetViewController, libraryViewController]
+  }
+
+  private func clearShadow() {
+    UITabBar.appearance().shadowImage = UIImage()
+    UITabBar.appearance().backgroundImage = UIImage()
+    UITabBar.appearance().backgroundColor = UIColor.white
+    UITabBar.appearance().layer.borderWidth = 0.0
   }
 }
