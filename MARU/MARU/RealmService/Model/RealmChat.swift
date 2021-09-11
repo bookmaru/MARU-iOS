f845b14db125c67de6a33f5d42a425b256a8fc5b
오준현 <oh.junhyeon95@gmail.com>
2021-09-11 13:54:00 +0900

    :rotating_light: force try lint 제거

---
+++ MARU/MARU/RealmService/Model/RealmChat.swift
@@ -0,0 +1,8 @@
+//
+//  RealmChat.swift
+//  MARU
+//
+//  Created by 오준현 on 2021/09/10.
+//
+
+import Foundation
---
+++ MARU/MARU/RealmService/RealmNotification.swift
@@ -0,0 +1,8 @@
+//
+//  RealmNotification.swift
+//  MARU
+//
+//  Created by 오준현 on 2021/09/11.
+//
+
+import Foundation
