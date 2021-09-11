a68587efb229a741938649d8c14a02e0f39fec53
오준현 <oh.junhyeon95@gmail.com>
2021-09-12 02:25:19 +0900

    🔥 필요없는 코드 제거

--- MARU/MARU/RealmService/RealmNotification.swift
+++ MARU/MARU/RealmService/RealmNotification.swift
@@ -16,17 +16,12 @@ final class RealmNotification {
 
   private init() { }
 
-  func subscribeRealm(roomID: Int) {
-
-  }
-
   func fetchObjectFromRealm(roomID: Int) -> PublishSubject<[RealmChat]> {
     let chatPublisher = PublishSubject<[RealmChat]>()
     token = RealmService.shared.read(roomID).observe { chages in
       switch chages {
       case let .update(chat, _, _, _):
         let chats: [RealmChat] = chat.map { $0 }
-        print("asdasd token", chats)
         chatPublisher.onNext(chats)
       default:
         break
