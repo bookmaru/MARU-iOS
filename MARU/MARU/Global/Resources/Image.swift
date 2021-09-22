9a90788fd5bfd975c5897e4e89773084bdb8ef44
yoonjinlee <yoonjinifer@gmail.com>
2021-09-22 19:17:40 +0900

    💄이미지파일 추가

--- MARU/MARU/Global/Resources/Image.swift
+++ MARU/MARU/Global/Resources/Image.swift
@@ -65,6 +65,8 @@ enum Image {
   static let invalidName2 = UIImage(named: "invalidName2")
   static let star1 = UIImage(named: "star1")
   static let star5 = UIImage(named: "star5")
-  static let group1011  = UIImage(named:"group1011")
+  static let group1011 = UIImage(named:"group1011")
   static let group1029 = UIImage(named:"group1029")
+  static let group1036 = UIImage(named: "group1036")
+  static let coolicon = UIImage(named: "coolicon")
 }
