1e931207c97fe65d91ca248a495f266edbe4d6f9
yoonjinlee <yoonjinifer@gmail.com>
2021-09-22 15:01:40 +0900

    ✨모임하고 싶은 책 검색 - 최근 검색어 저장 리스트 만드는 중

--- MARU/MARU/Screens/Library/ResultBookViewController.swift
+++ MARU/MARU/Screens/Library/ResultBookViewController.swift
@@ -7,11 +7,33 @@
 
 import UIKit
 
-class ResultBookViewController: BaseViewController {
+class ResultBookViewController: BaseViewController, UISearchBarDelegate {
+
+  enum Section {
+    case main
+  }
 
   private let searchBar = UISearchBar().then {
     $0.placeholder = "책 제목"
   }
+
+  private let cancelButton = UIBarButtonItem().then {
+    $0.tintColor = .black
+    $0.title = "취소"
+  }
+
+  private let activatorView = UIActivityIndicatorView().then {
+    $0.startAnimating()
+    $0.hidesWhenStopped = true
+  }
+
+  private var resultCollectionView: UICollectionView! = nil
+  private var resultDataSource: UICollectionViewDiffableDataSource<Section, MeetingModel>!
+  private var searchedKeyword: String! = nil
+  // TODO: - 책 검색하는 서버 연결
+  private var viewModel =  ResultSearchViewModel()
+  private var keyword: String! = nil
+
   override func viewDidLoad() {
     super.viewDidLoad()
     render()
@@ -29,12 +51,7 @@ extension ResultBookViewController {
     navigationItem.leftBarButtonItem = nil
     navigationItem.hidesBackButton = true
     navigationItem.titleView = searchBar
-    navigationItem.rightBarButtonItem = UIBarButtonItem(
-      title: "취소",
-      style: .plain,
-      target: self,
-      action: #selector(didTapCancelButton)
-    )
+    navigationItem.rightBarButtonItem = cancelButton
     navigationItem.rightBarButtonItem?.tintColor = .black
   }
 
