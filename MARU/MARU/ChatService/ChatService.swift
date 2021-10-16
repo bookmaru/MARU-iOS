c1a427ad5f53cc9b45989d1deb032e3bc11bd0cb
오준현 <oh.junhyeon95@gmail.com>
2021-10-17 00:21:12 +0900

    :ambulance: Chat Service 정리, background disconnect 이슈 해결

--- MARU/MARU/ChatService/ChatService.swift
+++ MARU/MARU/ChatService/ChatService.swift
@@ -15,16 +15,10 @@ final class ChatService {
 
   deinit { }
 
-  func start() {
-    register()
-    bindMessage()
-    bindChatList()
-  }
-
   private let baseURL = Enviroment.socketURL
   private let socket = StompClientLib()
   private let message = PublishSubject<String>()
-  private let disposeBag = DisposeBag()
+  private var disposeBag = DisposeBag()
   private let saveChatListRealmPublisher = PublishSubject<Int>()
 
   private var roomID: Int? {
@@ -38,45 +32,16 @@ final class ChatService {
   private var topic: String = ""
   private var messageDisposeBag = DisposeBag()
 
-  private func register() {
-    socket.openSocketWithURLRequest(
-      request: NSURLRequest(url: baseURL),
-      delegate: self as StompClientLibDelegate
-    )
+  func start() {
+    register()
+    bindMessage()
+    bindChatList()
+    repeatPing()
   }
+}
 
-  private func bindMessage() {
-    message.subscribe(onNext: { [weak self] message in
-      guard let self = self,
-            let roomID = self.roomID
-      else { return }
-      let chat: [String: Any] = [
-        "chatId": UUID().uuidString,
-        "roomId": roomID,
-        "userId": KeychainHandler.shared.userID,
-        "type": "CHAT",
-        "content": message,
-        "sender": UserDefaultHandler.shared.userName ?? "",
-        "time": self.realTime()
-      ]
-      self.socket.sendJSONForDict(
-        dict: chat as AnyObject,
-        toDestination: self.destination
-      )
-    })
-    .disposed(by: disposeBag)
-  }
-
-  private func bindChatList() {
-    saveChatListRealmPublisher
-      .flatMap { NetworkService.shared.group.chatList(roomID: $0) }
-      .subscribe(onNext: { [weak self] in
-        guard let self = self else { return }
-        self.saveChatRealm(chatList: $0)
-      })
-      .disposed(by: disposeBag)
-  }
-
+// MARK: - open method
+extension ChatService {
   func userRoomFinder(rooms: [Int]?) {
     guard let rooms = rooms else { return }
     rooms.forEach {
@@ -84,11 +49,18 @@ final class ChatService {
     }
   }
 
-  private func enrolledRoomSubcribe() {
-    let roomIDList = RealmService.shared.findRoomID()
-    roomIDList.forEach {
-      subscribeRoom(roomID: $0)
-    }
+  func disconnect() {
+    socket.disconnect()
+    disposeBag = DisposeBag()
+  }
+
+  func reconnect() {
+    bindChatList()
+    socket.reconnect(
+      request: NSURLRequest(url: baseURL),
+      delegate: self as StompClientLibDelegate
+    )
+    enrolledRoomSubcribe()
   }
 
   func bind(
@@ -116,7 +88,86 @@ final class ChatService {
   func clearMessageDisposeBag() {
     messageDisposeBag = DisposeBag()
   }
+}
 
+// MARK: - private method
+extension ChatService {
+  private func register() {
+    socket.openSocketWithURLRequest(
+      request: NSURLRequest(url: baseURL),
+      delegate: self as StompClientLibDelegate
+    )
+  }
+
+  private func repeatPing() {
+    Observable<Int>
+      .interval(.seconds(3), scheduler: MainScheduler.instance)
+      .observeOn(ConcurrentDispatchQueueScheduler(qos: .background))
+      .subscribe(onNext: { _ in
+        self.serverDidSendPing()
+      })
+      .disposed(by: disposeBag)
+  }
+
+  private func bindMessage() {
+    message.subscribe(onNext: { [weak self] message in
+      guard let self = self,
+            let roomID = self.roomID
+      else { return }
+      let chat: [String: Any] = [
+        "chatId": UUID().uuidString,
+        "roomId": roomID,
+        "userId": KeychainHandler.shared.userID,
+        "type": "CHAT",
+        "content": message,
+        "sender": UserDefaultHandler.shared.userName ?? "",
+        "time": self.realTime()
+      ]
+      self.socket.sendJSONForDict(
+        dict: chat as AnyObject,
+        toDestination: self.destination
+      )
+    })
+    .disposed(by: messageDisposeBag)
+  }
+
+  private func bindChatList() {
+    saveChatListRealmPublisher
+      .flatMap { NetworkService.shared.group.chatList(roomID: $0) }
+      .subscribe(onNext: { [weak self] in
+        guard let self = self else { return }
+        self.saveChatRealm(chatList: $0)
+      })
+      .disposed(by: disposeBag)
+  }
+
+  private func enrolledRoomSubcribe() {
+    let roomIDList = RealmService.shared.findRoomID()
+    roomIDList.forEach {
+      subscribeRoom(roomID: $0)
+    }
+  }
+
+  private func saveChatRealm(chatList: [RealmChat]) {
+    chatList.forEach {
+      RealmService.shared.write($0)
+    }
+  }
+
+  private func realTime() -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd.HH:mm:ss"
+    return dateFormatter.string(from: Date())
+  }
+
+  private func stringToTimeConvertor(string: String) -> Date {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd.HH:mm:ss"
+    return dateFormatter.date(from: string) ?? Date()
+  }
+}
+// MARK: - 채팅방 입장!
+extension ChatService {
   func joinRoom(roomID: Int) {
     socket.subscribe(destination: "/topic/public/\(roomID)")
     saveChatListRealmPublisher.onNext(roomID)
@@ -162,25 +213,6 @@ final class ChatService {
         toDestination: destination
       )
     }
-
-  }
-
-  private func saveChatRealm(chatList: [RealmChat]) {
-    chatList.forEach {
-      RealmService.shared.write($0)
-    }
-  }
-
-  private func realTime() -> String {
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "yyyy-MM-dd.HH:mm:ss"
-    return dateFormatter.string(from: Date())
-  }
-
-  private func stringToTimeConvertor(string: String) -> Date {
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "yyyy-MM-dd.HH:mm:ss"
-    return dateFormatter.date(from: string) ?? Date()
   }
 }
 
@@ -206,11 +238,9 @@ extension ChatService: StompClientLibDelegate {
     RealmService.shared.write(realm)
   }
 
-  func stompClientDidDisconnect(client: StompClientLib!) { }
+  func stompClientDidDisconnect(client: StompClientLib!) {  }
 
-  func stompClientDidConnect(client: StompClientLib!) {
-    enrolledRoomSubcribe()
-  }
+  func stompClientDidConnect(client: StompClientLib!) { enrolledRoomSubcribe() }
 
   func serverDidSendReceipt(client: StompClientLib!, withReceiptId receiptId: String) { }
 
@@ -218,9 +248,7 @@ extension ChatService: StompClientLibDelegate {
     client: StompClientLib!,
     withErrorMessage description: String,
     detailedErrorMessage message: String?
-  ) {
-    print("error", description)
-  }
+  ) { }
 
   func serverDidSendPing() { }
 }
