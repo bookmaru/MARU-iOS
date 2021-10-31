a7c9841df40afc66be92cc0d18615a770ae958ed
오준현 <oh.junhyeon95@gmail.com>
2021-10-31 17:14:06 +0900

    :necktie: 다이어리 에딧 기능 추가

--- MARU/MARU/Screens/MyDiary/DiaryWriteViewModel.swift
+++ MARU/MARU/Screens/MyDiary/DiaryWriteViewModel.swift
@@ -18,15 +18,25 @@ final class DiaryWriteViewModel {
   }
 
 	private let groupID: Int
+	private let isDiaryEdit: Bool
+	private let info: DiaryInfo?
 
-	init(groupID: Int) {
+	init(groupID: Int, info: DiaryInfo?, isDiaryEdit: Bool = false) {
 		self.groupID = groupID
+		self.isDiaryEdit = isDiaryEdit
+		self.info = info
 	}
 
   func transform(input: Input) -> Output {
 		let isSuccess = input.didTapDoneButton
-			.flatMap { NetworkService.shared.diary.postDiary(groupID: self.groupID, title: $0, content: $1)}
-			.map { $0.status == 201 }
+			.flatMap { [weak self] title, content -> Observable<BaseReponseType<Int>> in
+				guard let self = self else { return .empty() }
+				if self.isDiaryEdit {
+					return NetworkService.shared.diary.editDiary(groupID: self.info?.diaryID ?? 0, title: title, content: content)
+				}
+				return NetworkService.shared.diary.postDiary(groupID: self.groupID, title: title, content: content)
+			}
+			.map { $0.status == 201 || $0.status == 200 }
 
     return Output(isSuccess: isSuccess)
   }
