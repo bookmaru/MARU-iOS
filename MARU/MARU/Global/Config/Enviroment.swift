d8c62f2483a049bb8ce5198e6d84a86a4bc96fb9
Junhyeon <Oh.junhyeon95@gmail.com>
2022-01-31 15:54:42 +0900

    version up (#196)

--- MARU/MARU/Global/Config/Enviroment.swift
+++ MARU/MARU/Global/Config/Enviroment.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 struct Enviroment {
-  static let baseURL: URL = URL(string: "http://3.36.250.26:8080/api/v2/")!
+  static let baseURL: URL = URL(string: "http://3.36.248.213:8080/api/v2/")!
   static let socketURL: URL = URL(string: "ws://3.36.250.26:8082/ws/websocket")!
 }
